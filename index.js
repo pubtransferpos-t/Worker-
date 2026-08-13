b54f8d4f9f6472463146e0acf0adf9c5e478bb43
pubtransferpos-t <inballs129@gmail.com>
2026-08-13 16:06:26 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // You can view your logs in the Observability dashboard
-    console.info({ message: 'Hello World Worker received a request!' }); 
-    return new Response('Hello World!');
-  }
-};
